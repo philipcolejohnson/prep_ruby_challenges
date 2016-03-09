c91e0b2b1bc34efa4f2af21b18529eed5226cf41
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-03-08 19:06:32 -0800

    Adds game code, with challenges

--- counting_game.rb
+++ counting_game.rb
@@ -1,31 +1,48 @@
 def counting_game
   direction_up = true
   person_index = 0
-  friends = [[],[],[],[],[],[],[],[],[],[]] #Array.new(10, []) #
-  puts friends.to_s
+  friends = []
 
-  (1..100).each do |count|
-    friends[person_index] << count
-    person_index = change_person(person_index, count, direction_up)
+  puts "How many players?"
+  number_of_players = gets.chomp.to_i
+  puts "What should we count to?"
+  max_count = gets.chomp.to_i
+  puts
+
+  (1..max_count).each do |count|
+    #starting with one, take the current number and assign it the current person
+    friends << person_index
+
+    # check our count. if it was %7, then switch direction
+    direction_up = !direction_up if count % 7 == 0
+
+    #determine who the next person is, and make him the current person
+    person_index = change_person(person_index, direction_up, number_of_players)
+
+    #if it was % 11 count up or down twice
+    person_index = change_person(person_index, direction_up, number_of_players) if count % 11 == 0
+    
   end
 
   friends
 end
 
-def change_person (person_index, count, direction_up)
-  new_index = person_index==9 ? 0 : person_index + 1
+def change_person (person_index, direction_up, number_of_players)
+  if direction_up
+    new_index = (person_index==number_of_players - 1) ? 0 : person_index + 1
+  else
+    new_index = (person_index==0) ? number_of_players - 1 : person_index - 1
+  end  
+
   new_index
-
-
 end
 
-def print_person_count(places)
-  last=0
-  places.each_index do |person|
-    puts "Person #{person+1} counted: #{places[person].to_s}"
-    last = person if places[person].include?(100)==true
+def print_person_count(counter_list)
+  counter_list.each_index do |count|
+    puts "   \##{count+1} was counted by: Person #{(counter_list[count]+1).to_s}"
   end
-  puts "\nThe last person was #{last+1}."
+
+  puts "\nThe last person was Person #{((counter_list[counter_list.length-1])+1).to_s}."
 end
 
 print_person_count(counting_game)
